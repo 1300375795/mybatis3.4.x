ea0c568b98192885e7aba7c2fab2f131ab300eb3
yedeguo <1300375795@qq.com>
2021-03-08 09:44:49 +0800

    加上注释

--- src/main/java/org/apache/ibatis/type/TypeHandler.java
+++ src/main/java/org/apache/ibatis/type/TypeHandler.java
@@ -1,17 +1,17 @@
 /**
- *    Copyright 2009-2015 the original author or authors.
- *
- *    Licensed under the Apache License, Version 2.0 (the "License");
- *    you may not use this file except in compliance with the License.
- *    You may obtain a copy of the License at
- *
- *       http://www.apache.org/licenses/LICENSE-2.0
- *
- *    Unless required by applicable law or agreed to in writing, software
- *    distributed under the License is distributed on an "AS IS" BASIS,
- *    WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- *    See the License for the specific language governing permissions and
- *    limitations under the License.
+ * Copyright 2009-2015 the original author or authors.
+ * <p>
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ * <p>
+ * http://www.apache.org/licenses/LICENSE-2.0
+ * <p>
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
  */
 package org.apache.ibatis.type;
 
@@ -25,12 +25,45 @@ import java.sql.SQLException;
  */
 public interface TypeHandler<T> {
 
-  void setParameter(PreparedStatement ps, int i, T parameter, JdbcType jdbcType) throws SQLException;
+    /**
+     * 设置请求参数
+     *
+     * @param ps
+     * @param i
+     * @param parameter
+     * @param jdbcType
+     * @throws SQLException
+     */
+    void setParameter(PreparedStatement ps, int i, T parameter, JdbcType jdbcType) throws SQLException;
 
-  T getResult(ResultSet rs, String columnName) throws SQLException;
+    /**
+     * 根据列名称从结果集中获取相应的结果
+     *
+     * @param rs
+     * @param columnName
+     * @return
+     * @throws SQLException
+     */
+    T getResult(ResultSet rs, String columnName) throws SQLException;
 
-  T getResult(ResultSet rs, int columnIndex) throws SQLException;
+    /**
+     * 根据列的序号从结果集中获取相应的结果
+     *
+     * @param rs
+     * @param columnIndex
+     * @return
+     * @throws SQLException
+     */
+    T getResult(ResultSet rs, int columnIndex) throws SQLException;
 
-  T getResult(CallableStatement cs, int columnIndex) throws SQLException;
+    /**
+     * 根据列序号从可调用声明中获取相应的结果
+     *
+     * @param cs
+     * @param columnIndex
+     * @return
+     * @throws SQLException
+     */
+    T getResult(CallableStatement cs, int columnIndex) throws SQLException;
 
 }
