b5d3ce1054b6a51f6a88dbdf096d9079fe9b442c
yedeguo <1300375795@qq.com>
2021-05-24 10:25:59 +0800

    加上测试说明 加上备注

--- src/main/java/org/apache/ibatis/cache/decorators/FifoCache.java
+++ src/main/java/org/apache/ibatis/cache/decorators/FifoCache.java
@@ -1,17 +1,17 @@
 /**
- *    Copyright 2009-2017 the original author or authors.
- *
- *    Licensed under the Apache License, Version 2.0 (the "License");
- *    you may not use this file except in compliance with the License.
- *    You may obtain a copy of the License at
- *
- *       http://www.apache.org/licenses/LICENSE-2.0
- *
- *    Unless required by applicable law or agreed to in writing, software
- *    distributed under the License is distributed on an "AS IS" BASIS,
- *    WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- *    See the License for the specific language governing permissions and
- *    limitations under the License.
+ * Copyright 2009-2017 the original author or authors.
+ * <p>
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ * <p>
+ * http://www.apache.org/licenses/LICENSE-2.0
+ * <p>
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
  */
 package org.apache.ibatis.cache.decorators;
 
@@ -28,63 +28,111 @@ import org.apache.ibatis.cache.Cache;
  */
 public class FifoCache implements Cache {
 
-  private final Cache delegate;
-  private final Deque<Object> keyList;
-  private int size;
+    /**
+     * 被装饰缓存
+     */
+    private final Cache delegate;
 
-  public FifoCache(Cache delegate) {
-    this.delegate = delegate;
-    this.keyList = new LinkedList<Object>();
-    this.size = 1024;
-  }
+    /**
+     * 缓存的key集合
+     */
+    private final Deque<Object> keyList;
 
-  @Override
-  public String getId() {
-    return delegate.getId();
-  }
+    /**
+     * 先进先出缓存大小
+     */
+    private int size;
 
-  @Override
-  public int getSize() {
-    return delegate.getSize();
-  }
-
-  public void setSize(int size) {
-    this.size = size;
-  }
-
-  @Override
-  public void putObject(Object key, Object value) {
-    cycleKeyList(key);
-    delegate.putObject(key, value);
-  }
-
-  @Override
-  public Object getObject(Object key) {
-    return delegate.getObject(key);
-  }
-
-  @Override
-  public Object removeObject(Object key) {
-    return delegate.removeObject(key);
-  }
-
-  @Override
-  public void clear() {
-    delegate.clear();
-    keyList.clear();
-  }
-
-  @Override
-  public ReadWriteLock getReadWriteLock() {
-    return null;
-  }
-
-  private void cycleKeyList(Object key) {
-    keyList.addLast(key);
-    if (keyList.size() > size) {
-      Object oldestKey = keyList.removeFirst();
-      delegate.removeObject(oldestKey);
+    /**
+     * 构造函数
+     *
+     * @param delegate
+     */
+    public FifoCache(Cache delegate) {
+        this.delegate = delegate;
+        this.keyList = new LinkedList<Object>();
+        this.size = 1024;
     }
-  }
+
+    @Override
+    public String getId() {
+        return delegate.getId();
+    }
+
+    @Override
+    public int getSize() {
+        return delegate.getSize();
+    }
+
+    public void setSize(int size) {
+        this.size = size;
+    }
+
+    /**
+     * 放入缓存
+     * 首先将key放入循环缓存key集合中 如果缓存已经达到最大的数量 那么就按先进先出的策略淘汰缓存 然后再存入新的缓存
+     *
+     * @param key   Can be any object but usually it is a {@link CacheKey}
+     * @param value The result of a select.
+     */
+    @Override
+    public void putObject(Object key, Object value) {
+        cycleKeyList(key);
+        delegate.putObject(key, value);
+    }
+
+    /**
+     * 获取缓存
+     *
+     * @param key The key
+     * @return
+     */
+    @Override
+    public Object getObject(Object key) {
+        return delegate.getObject(key);
+    }
+
+    /**
+     * 删除缓存
+     *
+     * @param key The key
+     * @return
+     */
+    @Override
+    public Object removeObject(Object key) {
+        return delegate.removeObject(key);
+    }
+
+    /**
+     * 清除缓存
+     */
+    @Override
+    public void clear() {
+        delegate.clear();
+        keyList.clear();
+    }
+
+    /**
+     * 获取读写锁
+     *
+     * @return
+     */
+    @Override
+    public ReadWriteLock getReadWriteLock() {
+        return null;
+    }
+
+    /**
+     * 先进先出策略淘汰缓存
+     *
+     * @param key
+     */
+    private void cycleKeyList(Object key) {
+        keyList.addLast(key);
+        if (keyList.size() > size) {
+            Object oldestKey = keyList.removeFirst();
+            delegate.removeObject(oldestKey);
+        }
+    }
 
 }
